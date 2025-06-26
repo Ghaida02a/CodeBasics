7b80bc7f9e46ccb23bb1b34b0d1f242c3de85823
Ghaida02a <ggmskaj@gmail.com>
2025-06-26 14:29:25 +0400

    Add Java code

---
+++ CodeBasics/HelloWorld.java
@@ -0,0 +1,17 @@
+package CodeBasics;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello Code Creators");
+        System.out.println("Name: Ghaida Al-Jabri");
+        System.out.println("Date of birth: 16/03/2002");
+        System.out.println("Phone Number: 94755300");
+        System.out.println("Email Address: ggmskaj@gmail.com");
+        System.out.println("Country: Oman");
+        System.out.println("Education: Bachelor of Science in Computer Science");
+        System.out.println("Skills: Python, HTML, CSS, C++, Java, Git, TensorFlow, MS Office");
+        System.out.println("Languages: -Arabic: Native \n           -English: Intermediate to Advanced (IELTS 6.0)");
+
+    }
+}
