1ba79d32b59cc4bc069bbbde21cd8b37205277c3
Ghaida02a <ggmskaj@gmail.com>
2025-06-30 12:13:22 +0400

    Print my cv on cmd using Java

---
+++ CodeBasics/HelloWorldCreators.java
@@ -0,0 +1,24 @@
+public class HelloWorldCreators {
+
+    public static void main(String[] args){
+        System.out.println("Name: Ghaida Mohamed Al-Jabri");
+        System.out.println("Address: Muscat, Oman");
+        System.out.println("Phone: +968 9475 5300");
+        System.out.println("E-mail: ggmskaj@gmail.com");
+        System.out.println("Education: Bachelor of Science in Computer Science from German University of Technology in Oman, Muscat Graduating in 2024");
+        
+		System.out.println("Experience: Software developer (End-To-End), Global Computers company, Al-Khoud");
+        System.out.println(" (June 2023 to August 2023)");
+		
+		System.out.println("Certifications: \n -IC3 Certification \n -IELTS Band 6.0  \n -Introduction to HTML & CSS");
+		
+		System.out.println("Technical Skills: ");
+		System.out.println(" - Programming Languages: Python, Java, C++");
+        System.out.println(" - Web: HTML, CSS, Bootstrap");
+        System.out.println(" - Tools: Git, GitHub, Visual Studio Code, Google Colab");
+        System.out.println(" - Frameworks/Libraries: TensorFlow, OpenCV");
+        System.out.println(" - Database: MySQL");
+        
+		System.out.println("Languages: \n - Arabic (Native) \n - English (Fluent  IELTS 6.0)");
+    }
+}
